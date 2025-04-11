9847f278de942cae50a1df404758093d14a2b0ec
Henrik VT <commits@henrikvt.com>
2025-04-11 18:27:37 -0400

    rename pkl files
    migrate filenames. records.pkl will eventually become Records.pkl, but
    git is stupid with filename casing

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/henrikvtcodes/tungsten/cmd"
 
-//go:generate pkl-gen-go config/server-config.pkl
+//go:generate pkl-gen-go config/Server.pkl
 func main() {
 	cmd.Execute()
 }
