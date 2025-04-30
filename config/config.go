f1a54791127be78cf407c5bef9a63d482be050c9
Henrik VT <commits@henrikvt.com>
2025-04-30 11:31:13 -0400

    config defaults & validation stuff

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"github.com/carlmjohnson/truthy"
 	"os"
 )
@@ -13,4 +14,11 @@ type WrappedServerConfig struct {
 	DNSConfig  *Server
 	SocketPath string
 	ConfigPath string
+}
+
+func ValidateForwardConfig(fwc ForwardConfig) error {
+	if len(fwc.Ipv4Addresses) == 0 && len(fwc.Ipv6Addresses) == 0 {
+		return fmt.Errorf("must provide at least one IPv4 or IPv6 address")
+	}
+	return nil
 }
