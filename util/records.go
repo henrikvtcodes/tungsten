281a7c2e20ae8245e014ce2f9add0a682252ce29
Henrik VT <commits@henrikvt.com>
2025-05-08 13:30:20 -0400

    record helpers

--- util/records.go
+++ util/records.go
@@ -1,1 +1,32 @@
 package util
+
+import (
+	"github.com/miekg/dns"
+	"net"
+)
+
+// ARecord takes a slice of net.IPs and returns a slice of A RRs.
+func ARecord(zone string, ips []net.IP) []dns.RR {
+	var answers []dns.RR
+	for _, ip := range ips {
+		r := new(dns.A)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeA,
+			Class: dns.ClassINET, Ttl: 3600}
+		r.A = ip
+		answers = append(answers, r)
+	}
+	return answers
+}
+
+// AAAARecord takes a slice of net.IPs and returns a slice of AAAA RRs.
+func AAAARecord(zone string, ips []net.IP) []dns.RR {
+	var answers []dns.RR
+	for _, ip := range ips {
+		r := new(dns.AAAA)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeAAAA,
+			Class: dns.ClassINET, Ttl: 3600}
+		r.AAAA = ip
+		answers = append(answers, r)
+	}
+	return answers
+}
