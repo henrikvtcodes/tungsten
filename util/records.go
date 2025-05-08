4a8bb06f58b8a218701fe47e0b6accdb52d9215c
Henrik VT <commits@henrikvt.com>
2025-05-08 15:28:21 -0400

    ttl handling

--- util/records.go
+++ util/records.go
@@ -6,12 +6,12 @@ import (
 )
 
 // ARecord takes a slice of net.IPs and returns a slice of A RRs.
-func ARecord(zone string, ips []net.IP) []dns.RR {
+func ARecord(zone string, ips []net.IP, ttl uint32) []dns.RR {
 	var answers []dns.RR
 	for _, ip := range ips {
 		r := new(dns.A)
 		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeA,
-			Class: dns.ClassINET, Ttl: 3600}
+			Class: dns.ClassINET, Ttl: ttl}
 		r.A = ip
 		answers = append(answers, r)
 	}
@@ -19,13 +19,25 @@ func ARecord(zone string, ips []net.IP) []dns.RR {
 }
 
 // AAAARecord takes a slice of net.IPs and returns a slice of AAAA RRs.
-func AAAARecord(zone string, ips []net.IP) []dns.RR {
+func AAAARecord(zone string, ips []net.IP, ttl uint32) []dns.RR {
 	var answers []dns.RR
 	for _, ip := range ips {
 		r := new(dns.AAAA)
 		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeAAAA,
-			Class: dns.ClassINET, Ttl: 3600}
+			Class: dns.ClassINET, Ttl: ttl}
 		r.AAAA = ip
+		answers = append(answers, r)
+	}
+	return answers
+}
+
+func CnameRecord(zone string, targets []string, ttl uint32) []dns.RR {
+	var answers []dns.RR
+	for _, target := range targets {
+		r := new(dns.CNAME)
+		r.Hdr = dns.RR_Header{Name: zone, Rrtype: dns.TypeCNAME,
+			Class: dns.ClassINET, Ttl: ttl}
+		r.Target = target
 		answers = append(answers, r)
 	}
 	return answers
