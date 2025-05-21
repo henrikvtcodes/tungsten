61ba3ac78e247fb9581de60dfe3174f6580616be
Henrik VT <commits@henrikvt.com>
2025-05-20 23:32:39 -0400

    good enough for now i think

--- tungsten.nix
+++ tungsten.nix
@@ -13,8 +13,19 @@ buildGoApplication {
   modules = ./gomod2nix.toml;
 
   preBuild = ''
-  export PATH="$PATH:${pkgs.pkl}/bin"
-  ${pkl-go}/bin/pkl-gen-go config/Server.pkl
+  export XDG_CACHE_HOME="$TMPDIR/xdg-cache"
+  export XDG_DATA_HOME="$TMPDIR/xdg-data"
+  export JAVA_HOME="$TMPDIR/java"
+  export GRAALVM_HOME="$TMPDIR/graalvm"
+  export PKL_HOME="$TMPDIR/pkl-home"
+  mkdir -p "$PKL_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$JAVA_HOME" "$GRAALVM_HOME" "$TMPDIR/java-tmp" # Ensure the directory exists
+
+  export _JAVA_OPTIONS="-Djava.io.tmpdir=$TMPDIR/java-tmp"
+
+  ls -l $TMPDIR
+
+  export PKL_EXEC=${pkgs.pkl}/bin/pkl
+  ${pkl-go}/bin/pkl-gen-go --cache-dir $PKL_HOME  --generator-settings generator-settings.pkl --base-path github.com/henrikvtcodes/tungsten config/Server.pkl
   '';
 
   buildInputs = with pkgs; [
