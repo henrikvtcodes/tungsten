85bc166b39c8a71dd5ab4c7203b6038b909ea085
Henrik VT <commits@henrikvt.com>
2025-08-11 16:38:39 -0500

    delete generated pkl go code

--- config/ForwardConfig.pkl.go
+++
@@ -1,8 +0,0 @@
-// Code generated from Pkl module `henrikvtcodes.tungsten.config.Server`. DO NOT EDIT.
-package config
-
-type ForwardConfig struct {
-	Ipv4Addresses []string `pkl:"ipv4Addresses" json:"ipv4Addresses"`
-
-	Ipv6Addresses []string `pkl:"ipv6Addresses" json:"ipv6Addresses"`
-}
--- config/records/Record.pkl.go
+++
@@ -1,6 +0,0 @@
-// Code generated from Pkl module `henrikvtcodes.tungsten.config.Records`. DO NOT EDIT.
-package records
-
-type Record interface {
-	GetTtl() uint32
-}
